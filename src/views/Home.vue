f198ffe0ee65584d2b907ae9176c5c6fd3c35c13
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-24 00:23:56 +0700

    create home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <div>
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for meals"
+      />
+    </div>
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter in letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
 import store from "../store";
 
-const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWX".split("");
 </script>
