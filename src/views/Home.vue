50bacb374c4041da3d2a85bde8aa6a2d7854570a
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-24 06:29:55 +0700

    creating search from api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="flex flex-col p-8">
-    <div>
-      <input
-        type="text"
-        class="rounded border-2 border-gray-200 w-full"
-        placeholder="Search for meals"
-      />
-    </div>
+    
 
     <div class="flex justify-center gap-2 mt-2">
       <router-link
