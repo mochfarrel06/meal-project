f85083ab126988dae4e676c82e01f98410ebe52b
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-24 08:18:04 +0700

    add search by letter and by ingredient

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,5 @@
 <template>
-  <div class="flex flex-col p-8">
-    
-
-    <div class="flex justify-center gap-2 mt-2">
-      <router-link
-        :to="{ name: 'byLetter', params: { letter } }"
-        v-for="letter in letters"
-        :key="letter"
-      >
-        {{ letter }}
-      </router-link>
-    </div>
-
-    <pre>{{ ingredients }}</pre>
-  </div>
+  <div class="flex flex-col p-8"></div>
 </template>
 
 <script setup>
@@ -21,7 +7,6 @@ import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import axiosClient from "../axiosClient";
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWX".split("");
 const ingredients = ref([]);
 
 onMounted(async () => {
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>By Letter</div>
+  <div>
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter in letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWX".split("");
+</script>
