5836534bdeac0ac52ec54f4a1293327b89b3e257
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-24 07:58:19 +0700

    add view source and instructions

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="w-[800px] mx-auto p-8">
-    <pre>{{ meal }}</pre>
+  <div class="max-w-[800px] mx-auto p-8">
     <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
-    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="max-w-full" />
     <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
       <div>
         <strong class="font-bold">Category:</strong> {{ meal.strCategory }}
       </div>
       <div><strong class="font-bold">Area:</strong> {{ meal.strArea }}</div>
       <div><strong class="font-bold">Tags:</strong> {{ meal.strTags }}</div>
+    </div>
+
+    <div class="my-3">
+      {{ meal.strInstructions }}
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2">
@@ -32,6 +35,16 @@
           >
         </ul>
       </div>
+
+      <div class="mt-10">
+        <YouTubeButton :href="meal.strYoutube" />
+        <a
+          :href="meal.strSource"
+          target="_blank"
+          class="px-3 py-2 rounded border-2 bg-indigo-600 text-white border-indigo-600 hover:bg-indigo-700 transition-colors"
+          >View original source</a
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +53,7 @@
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import axiosClient from "../axiosClient";
+import YouTubeButton from "../components/YouTubeButton.vue";
 
 const route = useRoute();
 const meal = ref({});
